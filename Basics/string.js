e219bb18f29d84d6516f0e46b2a64ddfe9af1e53
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-23 20:29:04 +0530

    Indexing

--- Basics/string.js
+++ Basics/string.js
@@ -3,6 +3,15 @@ let msg = "   hello   ";
 let newMsg = msg.trim();
 console.log(newMsg);
 
+//UpperCase & LowerCase
 let str = "Random string"
 console.log(str.toUpperCase());
 console.log(str.toLowerCase());
+
+//Indexing
+let code = "Ilovecoding"
+console.log("Index of I is",code.indexOf("I"));
+console.log("Index of g is",code.indexOf("g"));
+console.log("Index of love is",code.indexOf("love"));
+console.log("Index of d is",code.indexOf("d"));
+console.log("Index of x is",code.indexOf("x"));
