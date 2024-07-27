916ccd55b81f0398374a0906da19c2fadfb217ce
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-27 23:56:40 +0530

    Arrays

---
+++ Arrays/app.js
@@ -0,0 +1,7 @@
+let info = ["name", 30, 40.8, "age"]
+console.log(info);
+console.log("first value:",info[0]);
+console.log("second value:",info[1]);
+console.log("third value:",info[2]);
+console.log("third value:",info[3]);
+console.log("array length:",info.length);
