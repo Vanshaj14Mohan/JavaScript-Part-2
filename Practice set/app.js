9066d6938a0d8bdd6f87f1e1d4bd7580ff726d9e
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-27 15:16:20 +0530

    Practice set

---
+++ Practice set/app.js
@@ -0,0 +1,11 @@
+//1.
+let msg = "help" //trim it and convert into uppercase
+ans = msg.trim().toUpperCase();
+console.log(ans);
+
+//2.
+string = "yourschools"
+console.log(string.slice(4,9));
+console.log(string.indexOf("sc"));
+console.log(string.replace("your", "our"));
+console.log(string.slice(4,10).replace("l", "a"));
