18d3f4af10d83c061fc738c224f38cf06a7a990f
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-28 23:56:04 +0530

    array methods

---
+++ Array Method/app.js
@@ -0,0 +1,10 @@
+let cars = ["audi", "bmw", "bugati", "ford", "ferrari"]
+console.log(cars);
+console.log(cars.push("xuv"));
+// cars.pop();
+// console.log(cars.pop());
+cars.pop();
+console.log(cars.pop());
+//shift & unshift operators
+cars.unshift();
+console.log("shift & unshift operators", cars.unshift());
