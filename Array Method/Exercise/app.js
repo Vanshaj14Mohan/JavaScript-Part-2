4f7ad55ab4d1e0a56cd2e3428d63c70a0c8abef7
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-29 23:15:50 +0530

    Exercise

--- Array Method/Exercise/app.js
+++ Array Method/Exercise/app.js
@@ -2,5 +2,13 @@
 // start: ["january", "july", "march", "august"]
 //final: ["july", "june", "march", "august"]
 let month = ["january", "july", "march", "august"]
+//alternate way
+// month.shift();
+// month.shift();
+// console.log(month);
+// month.unshift("june");
+// month.unshift("july");
+console.log(month);
 console.log("Before test", month);
-console.log(month.shift());
+console.log(month[0] = "july", month[1] = "june");
+console.log("After test case",month)
