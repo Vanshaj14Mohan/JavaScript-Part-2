a2d1b8fa84a255ef8fc8caaaf911b98a35783b09
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-29 23:35:59 +0530

    array methods

--- Array Method/Exercise/app.js
+++ Array Method/Exercise/app.js
@@ -12,3 +12,15 @@ console.log(month);
 console.log("Before test", month);
 console.log(month[0] = "july", month[1] = "june");
 console.log("After test case",month)
+
+//indexOf & includes method
+//for indexOf -> will print the indexOf the values
+let primary = ["red", "green", "blue", "yellow"]
+console.log(primary.indexOf("green"));
+console.log(primary.indexOf("yellow"));
+console.log(primary.indexOf("pink"));
+//For includes method
+//-> Will show the included values
+console.log(primary.includes("green"))//true
+console.log(primary.includes("pink"))//false
+
