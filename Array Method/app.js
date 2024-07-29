b20aec5e5eac95237b4c58a00279e669d86805d0
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-29 22:47:47 +0530

    Array Methods

--- Array Method/app.js
+++ Array Method/app.js
@@ -6,5 +6,15 @@ console.log(cars.push("xuv"));
 cars.pop();
 console.log(cars.pop());
 //shift & unshift operators
-cars.unshift();
-console.log("shift & unshift operators", cars.unshift());
+console.log("shift & unshift operators");
+cars.unshift("xuv");
+cars.unshift("thar");
+console.log(cars.unshift());
+console.log(cars);
+console.log(cars.shift());
+console.log(cars);
+
+let followers = ["a", "b", "c", "d"];
+console.log("current followers",followers);
+let blocked = followers.shift("a");
+console.log("blocked one",blocked);
