af9b914a0ba3e2324bf1b9c0416952fcf51f1b62
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-29 23:02:40 +0530

    Exercise

---
+++ Array Method/Exercise/app.js
@@ -0,0 +1,6 @@
+//1 -> For the given start state of an array, change it to final form using methods.
+// start: ["january", "july", "march", "august"]
+//final: ["july", "june", "march", "august"]
+let month = ["january", "july", "march", "august"]
+console.log("Before test", month);
+console.log(month.shift());
