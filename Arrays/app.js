ffe071989dd0d231109146af7386417a28bf53bd
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-30 22:12:02 +0530

    Arrays

--- Arrays/app.js
+++ Arrays/app.js
@@ -9,3 +9,14 @@ console.log("array length:",info.length);
 info[0] = "class"
 console.log("new info value:",info[0])
 console.log(info);
+
+//indexOf & includes method
+//for indexOf -> will print the indexOf the values
+let primary = ["red", "green", "blue", "yellow"]
+console.log(primary.indexOf("green"));
+console.log(primary.indexOf("yellow"));
+console.log(primary.indexOf("pink"));
+//For includes method
+//-> Will show the included values
+console.log(primary.includes("green"))//true
+console.log(primary.includes("pink"))//false
