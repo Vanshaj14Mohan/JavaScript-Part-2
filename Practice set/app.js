ba641c2b316ff2679be582531fb8e9ab308a6315
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-03 20:53:05 +0530

    Assignment

--- Practice set/app.js
+++ Practice set/app.js
@@ -22,3 +22,27 @@ let arr2 = [7,9,0,-2]
 let n = 3
 test2 = arr2.splice(arr2.length-n);
 console.log("New Case:", test2);
+
+//5 -> Wap to check wheter a string is blank or not.
+// let str = prompt("Enter a string");
+// if(str.length == 0){
+//     console.log("Empty string");
+// }
+// else{
+//     console.log("Not an empty string")
+// }
+
+//6 -> Wap to test whether character at the given place is lowercase or not.
+let name = "Amazing theme"
+let idx = 4;
+if(name[idx] == name[idx].toLowerCase()){
+    console.log("In lower case");
+}
+else{
+    console.log("Not in lower case");
+}
+
+//7 -> Wap to strip leading and trailing spaces from a string.
+let x = prompt("Enter a string");
+console.log("Original String", `${x}`);
+console.log("New string", `${str.trim()}`);
